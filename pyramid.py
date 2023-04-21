92ac1cb413d75470b7a4ec84b65895af9c60f02d
thejackedgar <thejackedgar@gmail.com>
2023-04-21 09:04:39 -0700

    add WIP pyramid file

---
+++ pyramid.py
@@ -0,0 +1,46 @@
+
+def descendingPyramid(rows, target, start=0, end=1, total=1, path=''):
+    if not rows:
+        return
+    print(rows, 'start:', start, ', end:', end)
+    # total = 0
+    # level = 0
+    row = rows[0]
+    for i in range(start, end):
+        thing = total*row[i]
+        print(i, 'spot:', row[i], ', total:', thing)
+        if rows[1:]:
+            # print('RECURSION')
+            rtn = descendingPyramid(rows[1:], target, i, i+2, thing)
+            print("rtn length:", len(rtn), "rows length:", len(rows))
+            if rtn:
+                if i%end == 0:
+                    path += 'L'
+                else:
+                    path += 'R'
+                path += rtn
+                return path
+        else:
+            print("reached end", thing, "i:", i, "end:", end)
+            if thing == target:
+                print('FOUND', path, row[i])
+                if i%end == 0:
+                    path += 'L'
+                else:
+                    path += 'R'
+                print("RETURNING PATH", path)
+                return path
+    return path
+    
+
+rows = [[1], [2, 3], [4, 1, 1]]
+target = 2
+
+result = descendingPyramid(rows, target)
+print("RESULT IS", result)
+
+rows2 = [[2], [4, 3], [3, 2, 6], [2, 9, 5, 2], [10, 5, 2, 15, 5]]
+target2 = 720
+
+result2 = descendingPyramid(rows2, target2)
+print("RESULT_2 IS", result2)
